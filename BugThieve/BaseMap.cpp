74548e053cf976c1f4fbf33619fb890adf92f659
vinhh <vinhanhlun@gmail.com>
2019-04-23 16:03:31 +0700

    Update BaseMap.cpp
    demo

--- BugThieve/BaseMap.cpp
+++ BugThieve/BaseMap.cpp
@@ -26,15 +26,6 @@ bool BaseMap::LoadMap(const std::string & map_data, const std::string & map_imag
 		solid_.push_back(arr);
 	}
 
-	//for (int i = 0; i < MAX_MAP_DATA_Y; i++)
-	//{
-	//	for (int j = 0; j < MAX_MAP_DATA_X; j++)
-	//	{
-	//		fscanf_s(fp, "%d", &x);
-	//		solid_[i].push_back(x);
-	//	}
-	//}
-
 	fclose(fp);
 	
 	bool ret = LoadImg(map_image, des);
@@ -45,44 +36,3 @@ std::vector<std::vector<int>> BaseMap::MapData()
 {
 	return solid_;
 }
-
-//void BaseMap::LoadThreats(const std::string &trap_image, const std::string & traps_list)
-//{
-//	FILE* fp = NULL;
-//	int amount_traps;
-//	int x_center;
-//	int y_center;
-//	int Radius;
-//	int angle;
-//	int frequency;
-//
-//	fopen_s(&fp, traps_list.c_str(), "rb");
-//	if (fp == NULL)
-//	{
-//		return;
-//	}
-//
-//	fscanf_s(fp, "%d", &amount_traps);
-//	for (int i = 0; i < amount_traps; i++)
-//	{
-//		fscanf_s(fp, "%d %d %d %d %d", &x_center, &y_center, &Radius, &angle, &frequency);
-//		TrapSaw saw(x_center, y_center, Radius, angle, frequency);
-//	}
-//	fclose(fp);
-//	
-//
-//}
-
-//bool BaseMap::Impact(int x_bug, int y_bug)
-//{
-//	for (int i = 0; i != trap_list_.size(); i++)
-//	{
-//		double d;
-//		d = sqrt(pow(trap_list_[i].x_coordinate_ - x_bug, 2) + pow(trap_list_[i].y_coordinate_ - y_bug, 2));
-//		if (d < COLLISION_RADIUS)
-//		{
-//			return true;
-//		}
-//	}
-//	return false;
-//}
